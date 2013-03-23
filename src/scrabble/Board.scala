a6e0f7ddb7d19e6dc3e07579b3b02a242163df9c
Gordon Martin <gordonhughmartin@gmail.com>
2013-03-23 19:09:00 +0000

    Added toString methods

--- src/scrabble/Board.scala
+++ src/scrabble/Board.scala
@@ -1,72 +1,78 @@
 package scrabble;
 
-case class Board (
-    squares: Map[Pos, Square]
-    
-	){
+case class Board(
+  squares: Map[Pos, Square]) {
 
-  
+  override def toString = {
+    List.range(1, 16).map { (x) => List.range(1, 16).map
+      { y => if (y == 15) this.squares.get(Pos.posAt(x, y).get).toString + "\n " else this.squares.get(Pos.posAt(x, y).get).toString } }.toString
+  }
 }
 
 object Board {
   import Pos._
-  
-  def apply(squares: Traversable[(Pos, Square)]): Board = Board(squares.toMap)
-  
-  def init : Board =
-  {
-    // Return a list of all the bonus squares. The rest of the board are then 'normal squares'
-    val bonusSquares : Map[(Int,Int), Square] =
-    {
-      val min:Int = 1
-      val max:Int = 15
-      
-      /* Quarter the board, then find the values from the rest of the board using the symmetry. I hope I can maths ;x..*/
-      val leftQuarter : List[((Int,Int), Square)] =
-      List( 
-          (1,1) -> TripleWordSquare(None),
-          (4,1) -> DoubleLetterSquare(None),
-          (8,1) -> TripleWordSquare(None),
-          (2,2) -> DoubleWordSquare(None),
-          (6,2) -> TripleLetterSquare(None),
-          (3,3) -> DoubleWordSquare(None),
-          (7,3) -> DoubleLetterSquare(None),
-          (1,4) -> DoubleLetterSquare(None),
-          (4,4) -> DoubleWordSquare(None),
-          (8,4) -> DoubleLetterSquare(None),
-          (5,5) -> DoubleWordSquare(None),
-          (2,6) -> TripleLetterSquare(None),
-          (6,6) -> TripleLetterSquare(None),
-          (3,7) -> DoubleLetterSquare(None),
-          (7,7) -> DoubleLetterSquare(None),
-          (1,8) -> TripleWordSquare(None),
-          (4,8) -> DoubleLetterSquare(None)
 
-          )
-       val rightQuarter: List[((Int,Int), Square)] = leftQuarter.map{ case ((x,y), square) => ((max + 1 -x, y), square) }
-       val bottomLeftQuarter: List[((Int,Int), Square)] = leftQuarter.map{ case ((x,y), square) => ((x, max + 1 - y), square) }
-       val bottomRightQuarter: List[((Int,Int), Square)] = bottomLeftQuarter.map{ case ((x,y), square) => ((max + 1 -x, y), square) }
-       
-       // Return all the special squares
-       (leftQuarter ::: rightQuarter ::: bottomLeftQuarter ::: bottomRightQuarter).toMap
-    } 
-    
-    // Construct the board. Anything that is not a bonus square is a NormalSquare
-    val all = Pos.all    
-    val list : List[(Pos,Square)] = for {
+  def apply(squares: Traversable[(Pos, Square)]): Board = Board(squares.toMap)
+
+  def init: Board =
+    {
+      // Return a list of all the bonus squares. The rest of the board are then 'normal squares'
+      val bonusSquares: Map[(Int, Int), Square] =
+        {
+
+          /* Quarter the board, then find the values from the rest of the board using the symmetry. I hope I can maths ;x..*/
+          val leftQuarter: List[((Int, Int), Square)] =
+            List(
+              (1, 1) -> TripleWordSquare(None),
+              (4, 1) -> DoubleLetterSquare(None),
+              (8, 1) -> TripleWordSquare(None),
+              (2, 2) -> DoubleWordSquare(None),
+              (6, 2) -> TripleLetterSquare(None),
+              (3, 3) -> DoubleWordSquare(None),
+              (7, 3) -> DoubleLetterSquare(None),
+              (1, 4) -> DoubleLetterSquare(None),
+              (4, 4) -> DoubleWordSquare(None),
+              (8, 4) -> DoubleLetterSquare(None),
+              (5, 5) -> DoubleWordSquare(None),
+              (2, 6) -> TripleLetterSquare(None),
+              (6, 6) -> TripleLetterSquare(None),
+              (3, 7) -> DoubleLetterSquare(None),
+              (7, 7) -> DoubleLetterSquare(None),
+              (1, 8) -> TripleWordSquare(None),
+              (4, 8) -> DoubleLetterSquare(None))
+
+          val rightQuarter: List[((Int, Int), Square)] = leftQuarter.map { case ((x, y), square) => ((Pos.max + 1 - x, y), square) }
+          val bottomLeftQuarter: List[((Int, Int), Square)] = leftQuarter.map { case ((x, y), square) => ((x, Pos.max + 1 - y), square) }
+          val bottomRightQuarter: List[((Int, Int), Square)] = bottomLeftQuarter.map { case ((x, y), square) => ((Pos.max + 1 - x, y), square) }
+
+          // Return all the special squares
+          (leftQuarter ::: rightQuarter ::: bottomLeftQuarter ::: bottomRightQuarter).toMap
+        }
+
+      // Construct the board. Anything that is not a bonus square is a NormalSquare
+      val all = Pos.all
+      val list: List[(Pos, Square)] = for {
         a <- all
-        (x,y) = a
+        (x, y) = a
         special = bonusSquares.get(x, y)
-        
-        square:Square = special match {
+
+        square: Square = special match {
           case None => NormalSquare(None)
           case Some(x) => x
         }
- 
-    } yield Pos.posAt(x:Int,y:Int).get -> square 
 
-    Board(list.toMap)
+      } yield Pos.posAt(x, y).get -> square
+
+      Board(list.toMap)
+    }
+
+  def main(args: Array[String]) {
+    val board = Board.init
+    println(board)
+
   }
-  
+
 }
+
+
 
--- src/scrabble/Pos.scala
+++ src/scrabble/Pos.scala
@@ -1,47 +1,48 @@
 package scrabble
 
-sealed case class Pos private (x:Int, y: Int, gridCordinates: String) {
-  
-	import Pos.posAt
-	
-	// Inspired by ScalaChess. A convenient way to access neighbouring squares.
-	lazy val up: Option[Pos] = posAt(x, y + 1)
-	lazy val down: Option[Pos] = posAt(x, y -1)
-	lazy val left: Option[Pos] = posAt(x - 1, y)
-	lazy val right: Option[Pos] = posAt(x + 1, y)
-	lazy val upLeft: Option[Pos] = up flatMap (_ left)
-    lazy val upRight: Option[Pos] = up flatMap (_ right)
-    lazy val downLeft: Option[Pos] = down flatMap (_ left)
-    lazy val downRight: Option[Pos] = down flatMap (_ right)
- 
-  
+sealed case class Pos private (x: Int, y: Int, gridCordinates: String) {
+
+  import Pos.posAt
+
+  // Inspired by ScalaChess. A convenient way to access neighbouring squares.
+  lazy val up: Option[Pos] = posAt(x, y + 1)
+  lazy val down: Option[Pos] = posAt(x, y - 1)
+  lazy val left: Option[Pos] = posAt(x - 1, y)
+  lazy val right: Option[Pos] = posAt(x + 1, y)
+  lazy val upLeft: Option[Pos] = up flatMap (_ left)
+  lazy val upRight: Option[Pos] = up flatMap (_ right)
+  lazy val downLeft: Option[Pos] = down flatMap (_ left)
+  lazy val downRight: Option[Pos] = down flatMap (_ right)
+
 }
 
 object Pos {
-	  
-  /** Returns the position object at location (x,y) if it exists, else returns None (if it is out of the bounds of the board) */
-  def posAt(x:Int, y:Int) : Option[Pos] = allPositions get (x,y)
-  
-  val all : List[(Int, Int)] = for {i <- List.range(1,16); j <- List.range(1,16)} yield i -> j
-  
-  lazy val allPositions : Map [(Int, Int), Pos] = 
-  {
-    
-    // Letters mapped to columns, for displaying the move log
-    val gridCoords = (List.range(1, 16) zip List.range('a','p')).toMap
-    
-    // Create the position objects and map them to their location tuple
-    val x = for {
-        a <- all 	      
-        (x,y) = a
-        letter = gridCoords.get(x).get 
-        pos = Pos(x,y, letter.toString() + y)
-    } yield (x,y) -> pos
+	val min: Int = 1
+    val max: Int = 15
 
-     x.toMap
-  }
-  
-  def main(args:Array[String]) {
+  /** Returns the position object at location (x,y) if it exists, else returns None (if it is out of the bounds of the board) */
+  def posAt(x: Int, y: Int): Option[Pos] = allPositions get (x, y)
+
+  val all: List[(Int, Int)] = for { i <- List.range(1, 16); j <- List.range(1, 16) } yield i -> j
+
+  lazy val allPositions: Map[(Int, Int), Pos] =
+    {
+
+      // Letters mapped to columns, for displaying the move log
+      val gridCoords = (List.range(1, 16) zip List.range('a', 'p')).toMap
+
+      // Create the position objects and map them to their location tuple
+      val x = for {
+        a <- all
+        (x, y) = a
+        letter = gridCoords.get(x).get
+        pos = Pos(x, y, letter.toString() + y)
+      } yield (x, y) -> pos
+
+      x.toMap
+    }
+
+  def main(args: Array[String]) {
     println(allPositions)
     println(allPositions.size)
   }
