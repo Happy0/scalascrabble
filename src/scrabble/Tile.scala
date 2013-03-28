943c648f1baee683c8a44ee19a51a50f7bea8d99
Gordon Martin <gordonhughmartin@gmail.com>
2013-03-28 12:22:08 +0000

    See previous commit

--- src/scrabble/Tile.scala
+++ src/scrabble/Tile.scala
@@ -1,7 +1,5 @@
 package scrabble
 
-import scala.collection.parallel.Foreach
-
 class Tile (letter: Char, value: Int) {
 
   
@@ -9,16 +7,19 @@ class Tile (letter: Char, value: Int) {
 }
 
 object Tile {
-  //@TODO: Add distributions for each letter to each tuple
-	val onePoints = List('E','A','I','O','N','R','T','L','S','U').map(ch => (ch,1))
-	val twoPoints = List('D','G').map(ch => (ch,2))
-	val threePoints = List('B','C','M','P').map(ch => (ch,3))
-	val fourPoints = List('F','H','V','W','Y').map(ch => (ch,4))
-	val fivePoints = List('K').map(ch => (ch,5))
-	val eightPoints = List('J','X').map(ch => (ch,8))
-	val tenPoints = List('Q','Z').map(ch => (ch,10))
+ 
+  
+	val blankPoints = List(('_',0,2))
+	val onePoints = List('E','A','I','O','N','R','T','L','S','U').map(ch => (ch,1))  //@TODO: Add distributions for each letter to each tuple
 	
-	val allValues = onePoints:::twoPoints:::threePoints:::fourPoints:::fivePoints:::eightPoints:::tenPoints
+	val twoPoints = List(('D',2,4),('G',2,3))
+	val threePoints = List('B','C','M','P').map(ch => (ch,3,2))
+	val fourPoints = List('F','H','V','W','Y').map(ch => (ch,4,2))
+	val fivePoints = List('K').map(ch => (ch,5,1))
+	val eightPoints = List('J','X').map(ch => (ch,8,1))
+	val tenPoints = List('Q','Z').map(ch => (ch,10,10))
+	
+	val allValues = blankPoints ::: onePoints:::twoPoints:::threePoints:::fourPoints:::fivePoints:::eightPoints:::tenPoints
 	
 	
 }
