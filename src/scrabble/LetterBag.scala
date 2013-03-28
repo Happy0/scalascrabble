531bd7e64ca7faf5c89f65b16d1f9cf83a05dbaa
Happy0 <gordonhughmartin@gmail.com>
2013-03-28 22:03:38 +0000

    Fixed circular definitions using apply methods. Hope I don't make that mistake again...

--- src/scrabble/LetterBag.scala
+++ src/scrabble/LetterBag.scala
@@ -1,16 +1,17 @@
 package scrabble
 
 /** tiles: The current tiles in the bag */
-class LetterBag(tiles: List[Letter]) {
+case class LetterBag(letters: List[Letter]) {
+
+  override def toString = letters.toString
 
 }
 
 object LetterBag {
 
-  def apply(tiles: List[Letter]): LetterBag = LetterBag(tiles)
-
-  /** Returns a new LetterBag in its intial state */
+  /** Returns a new LetterBag in its intial state. List is in randomised order. */
   val init: LetterBag = {
+
     val blankPoints = List(('_', 0, 2))
     val onePoints = List('E', 'A', 'I', 'O', 'N', 'R', 'T', 'L', 'S', 'U') zip List(12, 9, 9, 8, 6, 6, 6, 4, 4, 4) map { case (x, y) => (x, 1, y) }
     val twoPoints = List(('D', 2, 4), ('G', 2, 3))
@@ -20,15 +21,18 @@ object LetterBag {
     val eightPoints = List('J', 'X').map(ch => (ch, 8, 1))
     val tenPoints = List('Q', 'Z').map(ch => (ch, 10, 10))
 
-    val all = blankPoints ::: onePoints ::: twoPoints ::: threePoints ::: fourPoints ::: fivePoints ::: eightPoints ::: tenPoints
+    val all: List[(Char, Int, Int)] = blankPoints ::: onePoints ::: twoPoints ::: threePoints ::: fourPoints ::: fivePoints ::: eightPoints ::: tenPoints
 
-    // Yield a list of letters, using the distribution to yield the right number of letters
-    val letters = all.foldLeft(List.empty[Letter]){
-      case (list, (let,vl, dst)) => 
-        list ++ List.tabulate(dst)(i => Letter(let, vl))
-    }
-    
+    // Yield a list of all the letters in the bag, using the distribution to yield the right number of letters
+    val letters = all.foldLeft(List.empty[Letter]) { case (list, (chr: Char, vl: Int, dst: Int)) =>
+      list ::: List.fill(dst)(Letter(chr, vl)) }
+
+    // Construct with a randomised list
     LetterBag(util.Random.shuffle(letters))
   }
 
+  def main(args: Array[String]) {
+    val bag = LetterBag.init
+    println(bag)
+  }
 }
