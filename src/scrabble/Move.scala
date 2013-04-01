5c3c38278f1368a30ae2ef8ad7e1fcad25bf18f0
Gordon Martin <gordonhughmartin@gmail.com>
2013-04-02 00:56:41 +0100

    sfsdf

--- src/scrabble/Move.scala
+++ src/scrabble/Move.scala
@@ -31,54 +31,14 @@ case class Move(game: Game, placed: List[(Pos, Letter)], blanks: List[(Pos, Char
   val (starty, endy) = (placedSorted(0)._1.y, placedSorted(amountPlaced - 1)._1.y)
   val (horizontal, vertical) = (starty == endy, startx == endx)
 
-  // Returns true if the letter are placed in a legal distribution (linear or horizontal) within the board range, and it is attached to at least one existing word
-
-  //@TODO: Collect words as it goes
-  def validlyPlaced: Either[Boolean, InvalidMove] = {
-
-    if (!horizontal && !vertical) false else true
-
-    def isLinear: (Boolean, Boolean, Int, Int) = {
-
-      placedSorted.tail.foldLeft(true, false, startx, starty) {
-        case ((bl, neigh, lastx: Int, lasty: Int), (pos, let)) =>
-          val isLinear = if (horizontal) pos.y == lasty else pos.x == lastx
-          if (!isLinear) return (false, false, pos.x, pos.y)
-          val comesAfter = if (horizontal) pos.x == lastx + 1 else pos.y == lasty + 1
-
-          // Search for neighbouring squares
-          lazy val lookAt = pos.up :: pos.down :: pos.left :: pos.right :: List()
-
-          val hasNeighbours = if (neigh == true) true else !lookAt.find { ps => if (!ps.isDefined) false else !board.squareAt(ps.get).isEmpty }.isEmpty
-
-          if (comesAfter) (true, hasNeighbours, pos.x, pos.y) else {
-            // Loop from the current position to the previous position, making sure there are squares inbetween that the word is built from
-            val range = if (horizontal) List.range(lastx + 1, pos.x) else List.range(lasty + 1, pos.y)
-
-            val emptiesBetween = range.find { nxt =>
-              val curPos = if (horizontal) Pos.posAt(nxt, pos.y).get else Pos.posAt(pos.x, nxt).get
-              board.squareAt(curPos).isEmpty
-            }
-            println(emptiesBetween)
-
-            if (!emptiesBetween.isDefined) (true, true, pos.x, pos.y) else return (false, false, pos.x, pos.y)
-
-          }
-
-      }
-
-    }
-
-    val (bl, neigh, x, y) = isLinear
-
-    if (bl && neigh) Left(true) else if (!neigh && bl) return Right(NotAttachedToWord()) else Right(MisPlacedLetters(x, y))
-  }
-
+  // @TODO: Tidy this up, add anything after the end of the word 
   def buildWords: Either[List[List[(Pos, Char)]], InvalidMove] = {
     if (!horizontal && !vertical) Right(NotLinear()) else {
 
-      val startList = if (horizontal) board.LettersLeft(placedSorted(0)._1).map { case (pos, sq) => pos -> sq.tile.get.letter } else
-        board.LettersAbove(placedSorted(0)._1).map { case (pos, sq) => pos -> sq.tile.get.letter } :+ (placedSorted(0)._1, placedSorted(0)._2.letter)
+      val startList = (if (horizontal) board.LettersLeft(placedSorted(0)._1).map { case (pos, sq) => pos -> sq.tile.get.letter } else
+        board.LettersAbove(placedSorted(0)._1).map { case (pos, sq) => pos -> sq.tile.get.letter }) :+ (placedSorted(0)._1, placedSorted(0)._2.letter)
+
+      println("startList " + startList)
 
       val lists: (Int, Int, List[List[(Pos, Char)]]) = placedSorted.tail.foldLeft(startx, starty, List(startList)) {
         case ((lastx, lasty, (x :: xs)), (pos: Pos, let)) =>
@@ -94,14 +54,30 @@ case class Move(game: Game, placed: List[(Pos, Letter)], blanks: List[(Pos, Char
 
             val otherWords: List[(Pos, Char)] =
               if (horizontal) {
-                println("above: " + board.LettersAbove(pos))
-                println("below : " + board.LettersBelow(pos))
-                
-                (board.LettersAbove(pos).map { case (pos, sq) => pos -> sq.tile.get.letter } :+ pos -> let.letter) ::: board.LettersBelow(pos).map { case (pos, sq) => pos -> sq.tile.get.letter }
+                val above = board.LettersAbove(pos)
+                val below = board.LettersBelow(pos)
+                lazy val right = board.lettersRight(pos)
+
+                if (!above.isEmpty || !below.isEmpty) (above.map { case (ps, sq) => ps -> sq.tile.get.letter } :+ pos -> let.letter) ::: below.map { case (ps, sq) => ps -> sq.tile.get.letter } else {
+                  if ((pos.x, pos.y) == (startx, endx)){
+                    
+                  } else {
+                    List()
+                  }
+                }
+                  
+                 
               } else {
-                (board.LettersLeft(pos).map { case (pos, sq) => pos -> sq.tile.get.letter } :+ pos -> let.letter) ::: board.LettersRight(pos).map { case (pos, sq) => pos -> sq.tile.get.letter }
+                val left = board.LettersLeft(pos)
+                val right = board.LettersRight(pos)
+
+                if (!left.isEmpty || !right.isEmpty) (left.map { case (ps, sq) => ps -> sq.tile.get.letter } :+ pos -> let.letter) ::: right.map { case (ps, sq) => ps -> sq.tile.get.letter } else List()
               }
-            (pos.x, pos.y, updatedList :+ otherWords)
+            
+            
+
+            println("otherwords" + otherWords)
+            (pos.x, pos.y, if (!otherWords.isEmpty) updatedList :+ otherWords else updatedList)
 
           } else {
             val range = if (horizontal) List.range(lastx + 1, pos.x) else List.range(lasty + 1, pos.y)
@@ -113,9 +89,10 @@ case class Move(game: Game, placed: List[(Pos, Letter)], blanks: List[(Pos, Char
             if (!emptiesBetween.isDefined) {
               // Add the letters inbetween and the current char to the first list, then look for letters above and below the current char
               val between = range.map {
-                x =>
 
-                  val sq = if (horizontal) board.squareAt(Pos.posAt(pos.x, x).get) else Pos.posAt(x, pos.y).get
+                x =>
+                  val position = if (horizontal) Pos.posAt(x, pos.y) else Pos.posAt(pos.x, x)
+                  val sq = board.squareAt(position.get)
 
                   Pos.posAt(pos.x, x).get -> sq
               } :+ pos -> let.letter
@@ -125,11 +102,17 @@ case class Move(game: Game, placed: List[(Pos, Letter)], blanks: List[(Pos, Char
 
               val otherWords: List[(Pos, Char)] =
                 if (horizontal) {
-                  (board.LettersAbove(pos).map { case (pos, sq) => pos -> sq.tile.get.letter } :+ pos -> let.letter) ::: board.LettersBelow(pos).map { case (pos, sq) => pos -> sq.tile.get.letter }
+                  val above = board.LettersAbove(pos)
+                  val below = board.LettersBelow(pos)
+
+                  if (!above.isEmpty || !below.isEmpty) (above.map { case (ps, sq) => ps -> sq.tile.get.letter } :+ pos -> let.letter) ::: below.map { case (ps, sq) => ps -> sq.tile.get.letter } else List()
                 } else {
-                  (board.LettersLeft(pos).map { case (pos, sq) => pos -> sq.tile.get.letter } :+ pos -> let.letter) ::: board.LettersRight(pos).map { case (pos, sq) => pos -> sq.tile.get.letter }
+                  val left = board.LettersLeft(pos)
+                  val right = board.LettersRight(pos)
+
+                  if (!left.isEmpty || !right.isEmpty) (left.map { case (ps, sq) => ps -> sq.tile.get.letter } :+ pos -> let.letter) ::: right.map { case (ps, sq) => ps -> sq.tile.get.letter } else List()
                 }
-              (pos.x, pos.y, updatedList :+ otherWords)
+              (pos.x, pos.y, if (!otherWords.isEmpty) updatedList :+ otherWords else updatedList)
 
             } else {
               return Right(MisPlacedLetters(pos.x, pos.y))
@@ -138,7 +121,8 @@ case class Move(game: Game, placed: List[(Pos, Letter)], blanks: List[(Pos, Char
           }
 
       }
-        Left(lists._3)
+
+      Left(lists._3)
     }
 
   }
@@ -151,15 +135,15 @@ object Main {
 
     val board = Board.init
 
-    val newBrd = board.squares + (Pos.posAt(3, 2).get -> NormalSquare(Some(Letter('a', 1))))
+    val newBrd = board.squares + (Pos.posAt(1, 1).get -> NormalSquare(Some(Letter('a', 1))))
     val testBoard = Board(newBrd)
 
     println(testBoard)
 
     val placed = List(
+      Pos.posAt(4, 1).get -> Letter('A', 1),
       Pos.posAt(3, 1).get -> Letter('A', 1),
-      Pos.posAt(2, 1).get -> Letter('A', 1),
-      Pos.posAt(1, 1).get -> Letter('A', 1))
+      Pos.posAt(2, 1).get -> Letter('A', 1))
 
     val blanks = List()
 
