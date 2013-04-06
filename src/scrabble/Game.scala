a92e3dd06b9c19a169180a3eaa68570c06351eb1
Gordon Martin <gordonhughmartin@gmail.com>
2013-04-06 19:28:09 +0100

    Added some changes based on Ornicar's helpful code review. Remaining suggestions are listed in a gist.

--- src/scrabble/Game.scala
+++ src/scrabble/Game.scala
@@ -1,6 +1,6 @@
 package scrabble;
 
-case class Game(
+case class Game private (
   players: List[Player],
   dictionary: Dictionary,
   board: Board,
@@ -16,21 +16,26 @@ case class Game(
 
 object Game {
 
-  /** Initialises a new game, with a list of player names, with a language specific dictionary and letter bag  */
-  def init(playerNames: List[String], dictionary: Dictionary, letterbag: LetterBag): Game = {
+  /**
+   * Initialises a new game, with a list of player names, with a language specific dictionary and letter bag.
+   *  There must be between 2 and 4 players. Returns None if this condition isn't met.
+   */
+  def make(playerNames: List[String], dictionary: Dictionary, letterbag: LetterBag): Option[Game] = {
+    if (!(2 to 4 contains playerNames.size)) None else {
+      // Distribute letters from the bag to the players
+      val (players: List[Player], remainingBag: LetterBag) =
 
-    // Distribute letters from the bag to the players
-    val (players: List[Player], remainingBag: LetterBag) =
+        playerNames.foldLeft((List.empty[Player], letterbag)) {
+          case ((playerList, thebag), name) =>
+            val (letters: List[Letter], bag) = thebag.remove(7)
+            val player = Player(letters.map(x => Some(x)), name, 0)
 
-      playerNames.foldLeft((List.empty[Player], letterbag)) {
-        case ((playerList, thebag), name) =>
-          val (letters: List[Letter], bag) = thebag.remove(7)
-          val player = Player(letters.map(x => Some(x)), name, 0)
+            ((player :: playerList), bag)
+        }
 
-          ((player :: playerList), bag)
-      }
+      Some(Game(players, dictionary, Board.init, 0, remainingBag, 0))
+    }
 
-    Game(players, dictionary, Board.init, 0, remainingBag,0)
   }
 }
 
