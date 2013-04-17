9a1c9b8c08c00b3b42d49ae6fad98d53c45fff0d
Gordon Martin <gordonhughmartin@gmail.com>
2013-04-17 15:12:28 +0100

    y u no workie, sbt?

--- project/Build.scala
+++ project/Build.scala
@@ -3,12 +3,13 @@ import Keys._
 
 trait Resolvers {
   val sonatype = "sonatype" at "http://oss.sonatype.org/content/repositories/releases"
+  val snapshots = "snapshots" at "http://oss.sonatype.org/content/repositories/snapshots"
 }
 
 trait Dependencies {
   // val scalaz = "org.scalaz" % "scalaz-core_2.10.0-RC2" % "6.0.4"
   val scalaz = "org.scalaz" %% "scalaz-core" % "6.0.4"
-  val specs2 = "org.specs2" %% "specs2" % "1.14" % "test"
+  val specs2 = "org.specs2" %% "specs2" % "1.12" % "test"
 
 }
 
@@ -17,7 +18,7 @@ object ApplicationBuild extends Build with Resolvers with Dependencies {
   private val buildSettings = Project.defaultSettings ++ Seq(
     scalaVersion := "2.10.1",
     version := "1",
-    resolvers := Seq(sonatype),
+    resolvers := Seq(sonatype, snapshots),
     libraryDependencies := Seq(scalaz),
     libraryDependencies in test := Seq(specs2),
     scalacOptions := Seq("-deprecation", "-unchecked", "-feature", "-language:_")
