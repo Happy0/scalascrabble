7579dc1cc4aa58888a0b7bf18346522442767528
Gordon Martin <gordonhughmartin@gmail.com>
2013-05-28 02:26:17 +0100

    Struggling with Specs2. arghghgh.

--- src/test/scala/BoardTest.scala
+++ src/test/scala/BoardTest.scala
@@ -1,7 +1,31 @@
 package scrabble
+import org.specs2.matcher.ContainExactlyOnceMatcher
 
-class BoardTest {
-  
-  
+class BoardTest extends ScrabbleTest {
+
+  val board = Board.init
+
+  /** Place tiles on the board at the specified positions */
+  def placeSquares(placed: List[(Pos, Tile)]): Board = placed.foldLeft(board) {
+    case (b, placed) =>
+      b.placeLetter(placed._1, placed._2)
+  }
+
+  "a board" should {
+    
+    
+    "have 225 squares" in {
+      board.squares must have size (225)
+    }
+
+    "place 1 tile in" in {
+      board.placeLetter(Pos.posAt(3, 3).get, Letter('a', 1)
+          ).squares.toIterable must contain( (tup: (Pos, Square)) => tup._2.tile mustNotEqual None ).exactlyOnce 
+          
+    }
+
+    
+    
+  }
 
 }
--- src/test/scala/ScrabbleTest.scala
+++ src/test/scala/ScrabbleTest.scala
@@ -1,6 +1,19 @@
 package scrabble
-import org.specs2.mutable._
+import org.specs2.matcher.Matcher
+import org.specs2.mutable.Specification
+
+import scala.util.{ Try, Success, Failure }
+
 
 trait ScrabbleTest extends Specification {
+
+  /** A game which is in progress - or is 'constructed' to a certain scenario for the purposes of testing */
+  implicit def playedGame(game: Game) = new {
+	  
+    /** Place words on the board*/
+    def playBoardMoves(placed: List[(Pos, Tile)] *) : Try[Game] = ???
+    
+ 
+  }
 
 }
