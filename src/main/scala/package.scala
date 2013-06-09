1e5cdbafa715c1fbaabcb265f9375e94018e92b9
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-06-09 16:18:47 +0200

    remove unicode chars

---
+++ src/main/scala/package.scala
@@ -0,0 +1,11 @@
+package object scrabble extends scalaz.Zeros {
+
+  type PosTile = (Pos, Tile)
+  type PosTiles = List[PosTile]
+  type Direction = Pos ⇒ Option[Pos]
+
+  implicit final class ScrabblePimpedOption[A](o: Option[A]) {
+
+    def ??[B: scalaz.Zero](f: A => B): B = o.fold(mzero[B])(f)
+  }
+}
